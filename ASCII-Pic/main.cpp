b78c7468a534619a63e44addad20fd25cba1f61c
m-iDev-0792 <hezhenbang@macbook-pro.local>
2017-10-29 15:59:23 +0800

    Initial Commit

---
+++ ASCII-Pic/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ASCII-Pic
+//
+//  Created by 何振邦 on 2017/10/29.
+//  Copyright © 2017年 何振邦. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
